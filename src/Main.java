41627b316879e04e998e2dcc8dbf117d4dc26648
Margherita <margherita.deodato14@gmail.com>
2023-11-17 14:33:42 +0100

    commit AutoUnBoxing altra versione

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
  Scrivere una funzione che accetti in input 1 char lo stampi
  Scrivere una funzione che accetti in input 2 Integer e ne stampi la somma
  Scrivere una funzione che accetti in input 1 Character lo stampi
+
  Scrivere un un valore primitivo per int, double e char e fare autoboxing
  Scrivere un oggetto per Integer, Double e Character e fare Unboxing
  Usare le funzioni scritte con i valori creati*/
@@ -10,51 +11,44 @@
 public class Main {
 
     public static void main(String[] args) {
-        printSum(4, 10);
+        int risultato1 = printSum(40,50);
+        System.out.println(risultato1);
 
-        printChar('P');
+        Integer resAutobox1 = risultato1; //reaAutobox è variabile di tipo Integer
 
-        printSumInteger(16, 29);
 
-        printCharacter('S');
+        char risultato2 = printChar('Q');
+        System.out.println(risultato2);
+        Character resAutobox2 = risultato2;
 
-        int intValue = 56;
-        double doubleValue = 3.14;
-        char charValue = 'W';
 
-        Integer integerValue = intValue;
-        Double doubleWrapper = doubleValue;
-        Character charWrapper = charValue;
+        Integer risultato3 = printSumInteger(7,8);
+        System.out.println(risultato3);
+        int resUnbox3 = risultato3;
 
-        printSum(integerValue, 20);
-        printChar(charWrapper);
-        printSumInteger(5, integerValue);
-        printCharacter(charWrapper);
 
-        Integer objInteger = new Integer(38);
-        Double objDouble = new Double(7.3);
-        Character objCharacter = new Character('L');
+        Character risultato4 = printCharacter('H');
+        System.out.println(risultato4);
+        char resUnbox4 = risultato4;
 
-        int unboxedInt = objInteger;
-        double unboxedDouble = objDouble;
-        char unboxedChar = objCharacter;
 
-        printSum(unboxedInt, 7);
-        printChar(unboxedChar);
-        printSumInteger(13, objInteger);
-        printCharacter(unboxedChar);
+        double risultato5 = 3.4;
+        Double resAutobox5 = risultato5;
+        double resUnbox5 = resAutobox5;
+
+
     }
-    public static void printSum(int a, int b) {
-        System.out.println("Sum: " + (a + b));
+    public static int printSum(int a, int b) {
+      return a+b;
     }
-    public static void printChar(char c) {
-        System.out.println("Character: " + c);
+    public static char printChar(char c) {
+      return c;
     }
-    public static void printSumInteger(Integer x, Integer y) {
-        System.out.println("Sum Integer: " + (x + y));
+    public static Integer printSumInteger(Integer x, Integer y) {
+       return x+y;
     }
-    public static void printCharacter(Character ch) {
-        System.out.println("Character: " + ch);
+    public static Character printCharacter(Character ch) {
+       return ch;
     }
 }
 
